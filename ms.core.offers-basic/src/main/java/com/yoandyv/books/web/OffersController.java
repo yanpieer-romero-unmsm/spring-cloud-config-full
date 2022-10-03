cc609b44ea1f5b15cd6fa9a061848e31f964c9fd
Yanpieer Romero <yromeros@proveedor.intercorp.com.pe>
2022-10-02 19:50:58 -0500

    Adding new changes

--- ms.core.offers-basic/src/main/java/com/yoandyv/books/web/OffersController.java
+++ ms.core.offers-basic/src/main/java/com/yoandyv/books/web/OffersController.java
@@ -1,7 +1,6 @@
 package com.yoandyv.books.web;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
